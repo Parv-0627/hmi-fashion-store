866a90adfccd1b7df8a7b7802e3c424fe8ba5992
Parv-0627 <Parvezmalik@controlz.world>
2026-09-25 16:59:30 +0530

    Meesho style product cards + category heading

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,12 @@
 ﻿param([string]$msg = "Store update")
 
-# Downloads se sabse nayi index / admin file uthao
+# Downloads se file sirf tab uthao jab woh repo wali se NAYI ho
 foreach($name in @("index","admin")){
   $file = Get-ChildItem "$HOME\Downloads" -Filter "$name*.html" -ErrorAction SilentlyContinue |
           Sort-Object LastWriteTime -Descending | Select-Object -First 1
-  if($file){
-    Copy-Item $file.FullName ".\$name.html" -Force
+  $dest = ".\$name.html"
+  if($file -and ((-not (Test-Path $dest)) -or $file.LastWriteTime -gt (Get-Item $dest).LastWriteTime)){
+    Copy-Item $file.FullName $dest -Force
     Write-Host "[OK] $($file.Name) -> $name.html" -ForegroundColor Green
   }
 }
